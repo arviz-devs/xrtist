aa01d90da74c986abad038ad573eda556f56f3d1
Oriol (ZBook) <oriol.abril.pla@gmail.com>
2022-12-10 06:16:39 +0100

    add some tests

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,1 +1,34 @@
-import xrtist
+# pylint: disable=no-self-use, redefined-outer-name
+import pytest
+
+from xarray_einstats import tutorial
+from xrtist import PlotCollection
+
+
+@pytest.fixture(scope="module")
+def dataarray():
+    return tutorial.generate_mcmc_like_dataset(3)["mu"]
+
+
+class TestPlotCollectionInit:
+    def test_wrap(self, dataarray):
+        pc = PlotCollection.wrap(
+            dataarray, cols=["team"], aes={"color": ["chain"]}, color=[f"C{i}" for i in range(4)]
+        )
+        assert "plot" in pc.viz
+        assert "chart" in pc.viz
+        assert "chain" in pc.ds
+
+    def test_grid(self, dataarray):
+        pc = PlotCollection.grid(
+            dataarray,
+            cols=["team"],
+            rows=["chain"],
+            aes={"color": ["chain"]},
+            color=[f"C{i}" for i in range(4)],
+        )
+        assert "plot" in pc.viz
+        assert "team" in pc.viz["plot"].dims
+        assert "chain" in pc.viz["plot"].dims
+        assert "chart" in pc.viz
+        assert "chain" in pc.ds
